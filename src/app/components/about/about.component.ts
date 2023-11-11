aaa3bd14791a944d586e77a5eaae7506f306ed6f
Mxo01 <mariodimodica.01@gmail.com>
2023-11-11 18:12:47 +0100

    Install button fix

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -12,32 +12,57 @@ export class AboutComponent {
 	) as HTMLButtonElement;
 	installPrompt: any;
 
+	installButtonMobile: HTMLElement | undefined = document.getElementById(
+		"installMobile"
+	) as HTMLButtonElement;
+	installPromptMobile: any;
+
 	constructor(private router: Router) {}
 
 	ngOnInit(): void {
 		this.installButton = document.getElementById(
 			"install"
 		) as HTMLButtonElement;
+		this.installButtonMobile = document.getElementById(
+			"installMobile"
+		) as HTMLButtonElement;
 
 		window.addEventListener("beforeinstallprompt", event => {
 			event.preventDefault();
 			this.installPrompt = event;
 			this.installButton.removeAttribute("hidden");
+			this.installPromptMobile = event;
+			this.installButtonMobile.removeAttribute("hidden");
 		});
 	}
 
-	async installApp() {
-		if (!this.installPrompt) {
-			return;
+	async installApp(device: 'desktop' | 'mobile') {
+		if (device === 'desktop') {
+			if (!this.installPrompt) {
+				return;
+			}
+
+			const result = await this.installPrompt.prompt();
+			this.disableInAppInstallPrompt();
+			if (result.outcome === 'dismissed') window.location.reload();
 		}
-		const result = await this.installPrompt.prompt();
-		console.log(`Install prompt was: ${result.outcome}`);
-		this.disableInAppInstallPrompt();
+
+		else {
+			if (!this.installPromptMobile) {
+				return;
+			}
+
+			const result = await this.installPromptMobile.prompt();
+			this.disableInAppInstallPrompt();
+			if (result.outcome === 'dismissed') window.location.reload();
+		}
 	}
 
 	disableInAppInstallPrompt() {
 		this.installPrompt = null;
 		this.installButton.setAttribute("hidden", "");
+		this.installPromptMobile = null;
+		this.installButtonMobile.setAttribute("hidden", "");
 	}
 
 	backToHome() {
