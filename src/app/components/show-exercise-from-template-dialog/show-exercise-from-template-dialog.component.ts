cdc51caa0b067436de0075be5f5293b0cd372d65
Mario Di Modica <mariodimodica.01@gmail.com>
2023-12-07 11:30:44 +0100

    Fixed bug in show program

--- src/app/components/show-exercise-from-template-dialog/show-exercise-from-template-dialog.component.ts
+++ src/app/components/show-exercise-from-template-dialog/show-exercise-from-template-dialog.component.ts
@@ -8,6 +8,7 @@ import { Set } from "src/app/Models/Exercise.model";
 import { Workout } from "src/app/Models/Workout.model";
 import { formatSets } from "src/app/utils/utils";
 import { NotesDialogComponent } from "../notes-dialog/notes-dialog.component";
+import { TrainingProgram } from "src/app/Models/TrainingProgram.model";
 
 @Component({
 	selector: "app-show-exercise-from-template-dialog",
@@ -18,14 +19,16 @@ export class ShowExerciseFromTemplateDialogComponent {
 	public workout: Workout;
 
 	constructor(
-		@Inject(MAT_DIALOG_DATA) private data: any,
+		@Inject(MAT_DIALOG_DATA) private data: {workout: Workout},
 		private dialogRef: MatDialogRef<ShowExerciseFromTemplateDialogComponent>,
 		private dialog: MatDialog
 	) {}
 
 	ngOnInit() {
-		this.workout = this.data.workout;
-		console.log(this.workout);
+		this.workout = {
+            ...this.data.workout,
+            exercises: this.data.workout.exercises.filter((exercise) => exercise.template)
+        };
 	}
 
 	public formatSets(sets: Set[]) {
